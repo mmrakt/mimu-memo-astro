353548afc85272e5258c203892bb6e8553c4fa7c
mimu <akito.mimura@nijibox.co.jp>
2023-07-20 17:25:52 +0900

    fix vrt test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 const URL_LIST = [
   { url: '/page/1', name: 'index' },
@@ -7,19 +7,23 @@ const URL_LIST = [
   { url: '/qiita', name: 'qiita-index' },
   { url: '/owned/page/2', name: 'owned-index-page-2' },
 ]
+const SCREEN_TYPES = ['pc', 'sp']
 
 test.describe('スクリーンショットテスト', async () => {
   URL_LIST.forEach(async ({ url, name }) => {
-    for (const screenType of ['pc', 'sp']) {
+    for (const screenType of SCREEN_TYPES) {
       test(`${name}-${screenType}`, async ({ page }) => {
         if (screenType === 'sp') {
           await page.setViewportSize({ width: 480, height: 960 })
         }
         await page.goto(url)
-        expect(await page.screenshot()).toMatchSnapshot([
-          name,
-          `${name}-${screenType}.jpg`,
-        ])
+
+        // TODO: screenshot()の引数のpathでディレクトリ管理したいが、pathを指定すると修正前のUIで比較してしまう。要調査
+        // ex: path: `${__dirname}/index.spec.ts-snapshots/${name}/${screenType}.${IMG_EXTENSION}`,
+        const screenshot = await page.screenshot()
+
+        // ex: [name, `${screenType}.${IMG_EXTENSION}`]
+        expect(screenshot).toMatchSnapshot({ maxDiffPixelRatio: 0.2 })
       })
     }
   })
