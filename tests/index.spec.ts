1ffbec8c8f7dfe8c6501d596123a955d68793c3f
mimu <akito.mimura@nijibox.co.jp>
2023-07-20 15:03:51 +0900

    add vrt test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,9 +1,26 @@
-import { Page, expect } from '@playwright/test'
-import { test } from 'vitest'
+import { test, expect } from '@playwright/test'
 
-test('サイト名が表示される', async ({ page }: { page: Page }) => {
-  console.log(page)
-  await page.goto('/')
+const URL_LIST = [
+  { url: '/page/1', name: 'index' },
+  { url: '/page/2', name: 'index-page-2' },
+  { url: '/javascript-input', name: 'post' },
+  { url: '/qiita', name: 'qiita-index' },
+  { url: '/owned/page/2', name: 'owned-index-page-2' },
+]
 
-  await expect(page).toHaveTitle('mimu-memo')
+test.describe('スクリーンショットテスト', async () => {
+  URL_LIST.forEach(async ({ url, name }) => {
+    for (const screenType of ['pc', 'sp']) {
+      test(`${name}-${screenType}`, async ({ page }) => {
+        if (screenType === 'sp') {
+          await page.setViewportSize({ width: 480, height: 960 })
+        }
+        await page.goto(url)
+        expect(await page.screenshot()).toMatchSnapshot([
+          name,
+          `${name}-${screenType}.jpg`,
+        ])
+      })
+    }
+  })
 })
